88efafcb541538955aef6971a4cf666a05030000
Mihail Fomin PC <mihanaz098@gmail.com>
2026-09-01 17:38:50 +0300

    Add docker

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from app.api.v1.booking import router
+from app.core.init_db import init_db
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Создаём таблицы при старте
+    await init_db()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(router, prefix='/api/v1')
